f01a56669524e9ec884162f399aca397317902f2
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2020-07-18 23:50:17 +0900

    up

---
+++ RhythmGame/Assets/Script/NoteManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    public int bpm = 0; // 1분당 비트 수
+    double currentTime = 0f;
+
+    [SerializeField] Transform tfNoteApper = null;
+    [SerializeField] GameObject goNote = null;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if (currentTime >= 60d / bpm)
+        {
+            GameObject t_note = Instantiate(goNote, tfNoteApper.position, Quaternion.identity, transform);
+            currentTime -= 60d / bpm; // --> 조금의 시간이 지나가기 때문에 이걸 0으로 초기화하면 안 됨
+        }
+
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Note"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
